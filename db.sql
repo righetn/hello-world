5b360fc8e50bac0201704775296ea6ea407b8415
Nicolas Righetti <nicolasrighetti@MacBook-Pro-de-Nicolas.local>
2020-01-20 18:56:27 +0100

    fix sql script

--- db.sql
+++ db.sql
@@ -2,18 +2,18 @@ ALTER USER postgres PASSWORD '123';
 CREATE DATABASE "mydb";
 \c "mydb";
 CREATE TABLE "user" (
-    id integer PRIMARY_KEY,
-    name varchar(40)
+    id SERIAL PRIMARY KEY,
+    name varchar(40) NOT NULL
 );
 
 CREATE FUNCTION postUser(IN userName text) returns void as 
 $$
-    INSERT INTO user(id, name) VALUES (default, userName);
+    INSERT INTO "user" (name) VALUES (userName);
 $$
 LANGUAGE SQL;
 
 CREATE FUNCTION getUsers(OUT name varchar(40)) returns varchar as 
 $$
-    SELECT name FROM user;
+    SELECT name FROM "user";
 $$
 LANGUAGE SQL;

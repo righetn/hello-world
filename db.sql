c3720aaaf79bc2ee28eda6302ffa81a59f2bb3bd
Nicolas Mulet <nicolas.mulet@epita.fr>
2020-01-20 18:46:29 +0100

    updated sql procstocks

--- db.sql
+++ db.sql
@@ -5,3 +5,15 @@ CREATE TABLE "user" (
     id integer PRIMARY_KEY,
     name varchar(40)
 );
+
+CREATE FUNCTION postUser(IN userName text) returns void as 
+$$
+    INSERT INTO user(id, name) VALUES (default, userName);
+$$
+LANGUAGE SQL;
+
+CREATE FUNCTION getUsers(OUT name varchar(40)) returns varchar as 
+$$
+    SELECT name FROM user;
+$$
+LANGUAGE SQL;
